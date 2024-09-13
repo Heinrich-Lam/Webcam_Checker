e6df04d838820218625591ce7bcd18d5233ce480
kargo\heinrich <Br!nk$0008015058087>
2024-09-13 09:51:04 +0200

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace Webcam_Checker
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pbWebcam = new System.Windows.Forms.PictureBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.cbCamera = new System.Windows.Forms.ComboBox();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pbWebcam)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pbWebcam
+            // 
+            this.pbWebcam.Location = new System.Drawing.Point(14, 16);
+            this.pbWebcam.Name = "pbWebcam";
+            this.pbWebcam.Size = new System.Drawing.Size(481, 363);
+            this.pbWebcam.TabIndex = 0;
+            this.pbWebcam.TabStop = false;
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.pbWebcam);
+            this.panel1.Location = new System.Drawing.Point(82, 98);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(513, 394);
+            this.panel1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(204, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(271, 37);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Webcam Checker";
+            // 
+            // cbCamera
+            // 
+            this.cbCamera.FormattingEnabled = true;
+            this.cbCamera.Location = new System.Drawing.Point(211, 58);
+            this.cbCamera.Name = "cbCamera";
+            this.cbCamera.Size = new System.Drawing.Size(249, 21);
+            this.cbCamera.TabIndex = 3;
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(486, 56);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 4;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.Location = new System.Drawing.Point(597, 55);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(75, 23);
+            this.btnStop.TabIndex = 5;
+            this.btnStop.Text = "Stop";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(707, 504);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.cbCamera);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Webcam";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pbWebcam)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pbWebcam;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cbCamera;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnStop;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using AForge.Video;
+using AForge.Video.DirectShow;
+
+namespace Webcam_Checker
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        FilterInfoCollection filterInfoCollection;
+        VideoCaptureDevice videoCaptureDevice;
+        //On form load the first selected video device will be used, and it will be started automatically.
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            #region "Combo Selection with default name."
+            ////The following code will list all of the Video devices connected to the System.
+            //filterInfoCollection = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+            //foreach (FilterInfo filterInfo in filterInfoCollection)
+            //{//Adds all of the Items to the Combobox.
+            //    cbCamera.Items.Add(filterInfo);
+            //}
+            //cbCamera.SelectedIndex = 0;
+            ////This creates a new instance of the VideoCaptureDevice.
+            //videoCaptureDevice = new VideoCaptureDevice();
+
+            ////This creates a new instance of the VideoCaptureDevice.
+            ////Also retrieves the selected object from the FilterInfoCollection, based on the currently selected index from the combobox.
+            ////MonikerString: this contains the unique id for the selected Video Capture device.
+            //videoCaptureDevice = new VideoCaptureDevice(filterInfoCollection[cbCamera.SelectedIndex].MonikerString);
+
+            ////NewFrame is an event that fires whenever a new frame is captured from the video source.
+            ////this line attaches the 'VideoCaptureDevice_NewFrame' method as the event handler for this event.
+            //videoCaptureDevice.NewFrame += VideoCaptureDevice_NewFrame;
+
+            ////Starts capturing frames from the selected video source.
+            //videoCaptureDevice.Start();
+            #endregion
+
+            #region "Device Name."
+
+            filterInfoCollection = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+            foreach (FilterInfo filterInfo in filterInfoCollection)
+            {
+                try
+                {
+                    //Gets all of the Connected Video Devices from the system.
+                    //Adds all of the Device names to the System.
+                    string deviceName = GetDeviceName(filterInfo);
+                    cbCamera.Items.Add(deviceName);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Error extracting Device name: {ex.Message}");
+                }
+
+                //Automatically selects the first device in the Combobox.
+                if (cbCamera.Items.Count > 0)
+                {
+                    cbCamera.SelectedIndex = 0;
+                }
+                else
+                {
+                    MessageBox.Show("No video devices detected.");
+                }
+
+                try
+                {
+                    //If there is a selected device, the following will happen.
+                    if (cbCamera.SelectedIndex != null)
+                    {
+                        //Gets the string value from the selected combo value and sends it to the 'GetMonikerString' method.
+                        string selectedDeviceName = cbCamera.SelectedItem.ToString();
+                        videoCaptureDevice = new VideoCaptureDevice(GetMonikerString(selectedDeviceName));
+                    }
+                    else
+                    {
+                        throw new Exception("No device selected.");
+                    }
+
+                    //NewFrame is an event that fires whenever a new frame is captured from the video source.
+                    //this line attaches the 'VideoCaptureDevice_NewFrame' method as the event handler for this event.
+                    videoCaptureDevice.NewFrame += VideoCaptureDevice_NewFrame;
+                    //Starts capturing frames from the selected video source.
+                    videoCaptureDevice.Start();
+
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Error initializing Video Capture Device : {ex.Message}");
+                }
+
+            }
+            //Method to get all of the connected devices' names to populate the Combo.
+            string GetDeviceName(FilterInfo filterInfo)
+            {
+                return filterInfo.Name.Split(',')[0];
+            }
+
+            //Also retrieves the selected object from the FilterInfoCollection, based on the currently selected index from the combobox.
+            //MonikerString: this contains the unique id for the selected Video Capture device.
+            string GetMonikerString(string deviceName)
+            {
+                foreach (FilterInfo info in filterInfoCollection)
+                {
+                    if (info.Name.StartsWith(deviceName))
+                    {
+                        //This returns the unique ID of the selected Video Capture Device.
+                        return info.MonikerString;
+                    }
+                }
+                throw new Exception($"Device '{deviceName}' not found");
+            }
+
+            #endregion
+
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            //This creates a new instance of the VideoCaptureDevice.
+            //Also retrieves the selected object from the FilterInfoCollection, based on the currently selected index from the combobox.
+            //MonikerString: this contains the unique id for the selected Video Capture device.
+            videoCaptureDevice = new VideoCaptureDevice(filterInfoCollection[cbCamera.SelectedIndex].MonikerString);
+
+            //NewFrame is an event that fires whenever a new frame is captured from the video source.
+            //this line attaches the 'VideoCaptureDevice_NewFrame' method as the event handler for this event.
+            videoCaptureDevice.NewFrame += VideoCaptureDevice_NewFrame;
+
+            //Starts capturing frames from the selected video source.
+            videoCaptureDevice.Start();
+        }
+
+        private void VideoCaptureDevice_NewFrame(object sender, NewFrameEventArgs eventArgs)
+        {
+            //Sets the image for the display, calling the Frame to update the display in real-time.
+            pbWebcam.Image = (Bitmap)eventArgs.Frame.Clone();
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            //Checking if the videoCaptureDevice is running or not.
+            if (videoCaptureDevice.IsRunning == true)
+            {
+                //This stops the device from sending through images, and disposes of the resources that was captured.
+                videoCaptureDevice.Stop();
+                pbWebcam.Image = null;
+            }
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            //Checking if the videoCaptureDevice is running or not.
+            if (videoCaptureDevice.IsRunning == true)
+            {
+                //This stops the device from sending through images, and disposes of the resources that was captured.
+                videoCaptureDevice.Stop();
+                pbWebcam.Image = null;
+            }
+        }
+    }
+}
